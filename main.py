433506789d1480aa077710a31f2843a6a5e8def2
yelnurdo <yelnurdos@gmail.com>
2024-05-25 23:06:10 +0500

    update

---
+++ main.py
@@ -0,0 +1,7 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+from pathlib import Path
+
+# Load pages
+
+st.set_page_config(page_title="AI Recipe App", page_icon="👋", layout="wide")
--- pages/home_page.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.write("# Welcome to AI Recipe App")
-st.sidebar.success("Select a demo above.")
